f5b821afa0652888493f24c906d1b70621f7895a
vxcute <freemasry@proton.me>
2023-09-25 11:34:22 +0300

    haha

--- slock/config.mk
+++ slock/config.mk
@@ -7,8 +7,8 @@ VERSION = 1.5
 PREFIX = /usr/local
 MANPREFIX = ${PREFIX}/share/man
 
-X11INC = /usr/local/include
-X11LIB = /usr/local/lib
+X11INC = /usr/X11R6/include
+X11LIB = /usr/X11R6/lib
 
 # includes and libs
 INCS = -I. -I/usr/include -I${X11INC}
